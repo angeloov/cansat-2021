4d2f94b17299374623bcf9b9e1f25a0b875846a0
kirito012 <33160578+kirito012@users.noreply.github.com>
2021-05-01 19:21:05 +0200

    Update main.py

--- ServerCansat/main.py
+++ ServerCansat/main.py
@@ -1,56 +1,51 @@
 from flask import Flask, render_template 
 from flask_socketio import SocketIO , send, emit
 import random
-<<<<<<< HEAD
-=======
-import psycopg2
+import mysql.connector
+from mysql.connector import errorcode
 
-user = "postgres"
-password = "Frecciax77"
-
-con = psycopg2.connect(
-    host="localhost",
-    database="postgres",
-    user=user,
-    password=password)
->>>>>>> 52f4bb64c8c8198f48c543b8575b361478e63eb9
+dbCansat = mysql.connector.connect(
+    host='127.0.0.1',
+    user='root',
+    passwd='cansat',
+    database='CansatData'
+)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
-<<<<<<< HEAD
-@app.route("/")
-def home():
-    socketio.emit('message', "heyyyyy")
-=======
-curs = con.cursor()
-curs.execute("select exists(select * from cansat.tables where Data=%s)", ('Data',))
-if cur.fetchone()[0] == False :
-    curs.execute("CREATE TABLE Data (Temperatura int);")
+curs = dbCansat.cursor()
+curs.execute("select exists(select * from CansatData.data where temperatura=%s)", ('data',))
+if curs.fetchone()[0] == None :
+    curs.execute("CREATE TABLE data (temperatura int,areaatterraggio longtext,pressione int,gps longtext );")
 
 @app.route("/")
 def home():
->>>>>>> 52f4bb64c8c8198f48c543b8575b361478e63eb9
     return render_template("/home.html")
 
 @app.route("/stats")
 def stats():
     return render_template("/statistics.html")
 
-<<<<<<< HEAD
-# @socketio.on('message')
-# def UpdateEvent():
-    # emit('update',random.randrange(0,40))
-=======
 @socketio.on('my event')
 def handle_my_custom_event(json):
     print('received json: ' + str(json))
     emit('change value', {'data' : random.randrange(0,40)})
-    cur = con.cursor()
-    cur.execute("""SELECT datname from pg_database""")
-    rows = cur.fetchall()
->>>>>>> 52f4bb64c8c8198f48c543b8575b361478e63eb9
+    comando = "INSERT INTO cansatdata.data"
+    elencoCampi = " (temperatura,areaatterraggio,pressione,gps)"
+    elencoValori = " VALUES (" + str(random.randrange(0,40)) + " , " + "'0,1,2'" + " , " + str(random.randrange(0,40)) + " , " + "'32,42,377'" + ");"
+
+    query = comando + elencoCampi + elencoValori
+
+    curs.execute(query)
+
+    curs.execute("SELECT * FROM cansatdata.data")
+  
+    result = curs.fetchall()
+    
+    for row in result:
+        print(row, '\n')
 
 if __name__ == "__main__":
     socketio.run(app)
