263eb688b8e8a87db47f1ebc1c8b0ef450598567
angeloov <voicuangelo04@gmail.com>
2021-05-01 20:40:50 +0200

    Aggiunto componente Header

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -8,22 +8,21 @@ import Altitude from "./components/Altitude.js";
 import AirTemperature from "./components/AirTemperature";
 import Status from "./components/ConnectionStatus";
 import LaunchStatus from "./components/LaunchStatus";
-import Title from "./components/Title.js";
-import Uv from "./components/Uv.js";
-import Pressure from "./components/Pressure.js";
+import Uv from "./components/Uv";
+import Header from "./components/Header";
+import Pressure from "./components/Pressure";
 
 const App = () => {
   return (
     <div>
+      <Header />
+
       <div className="quadrants-container">
         <Altitude value="20" />
         <AirTemperature value="30" />
         <Pressure value={20} />
         <Uv value={20} />
-        {/* 
-      <Status />
-
-      <Title /> */}
+        {/* <Status /> */}
       </div>
     </div>
   );
---
+++ web-app/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div>
+      <h1 className="header-title">CanDashboard</h1>
+      <h2 className="header-subtitle">Oli 3AI Team</h2>
+    </div>
+  );
+}
