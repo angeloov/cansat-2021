190ba1deda8b30c298df627cbaa5bfb78084baea
angeloov <voicuangelo04@gmail.com>
2021-05-14 17:11:07 +0200

    Collegato il server python con la web app

--- server/main.py
+++ server/main.py
@@ -1,41 +1,78 @@
 from flask import Flask, render_template 
 from flask_socketio import SocketIO , send, emit
+import time
+import math
 import random
-import psycopg2
+from flask_cors import CORS
 
-user = "postgres"
-password = "Frecciax77"
+#import serial
+#import mysql.connector
+#from mysql.connector import errorcode
 
-con = psycopg2.connect(
-    host="localhost",
-    database="CansatData",
-    user=user,
-    password=password)
+#dbCansat = mysql.connector.connect(
+#    host='127.0.0.1',
+#    user='root',
+#    passwd='cansat',
+#   database='CansatData'
+#)
 
 app = Flask(__name__)
+CORS(app) # enable cors for * domains
+
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+#curs = dbCansat.cursor()
+#curs.execute("select exists(select * from CansatData.data where temperatura=%s)", ('data',))
+#if curs.fetchone()[0] == None :
+#    curs.execute("CREATE TABLE data (temperatura int,areaatterraggio longtext,pressione int,gps longtext );")
 
 @app.route("/")
 def home():
+    return "HEYY"
     return render_template("/home.html")
 
 @app.route("/stats")
 def stats():
     return render_template("/statistics.html")
 
-@socketio.on('my event')
-def handle_my_custom_event(json):
-    print('received json: ' + str(json))
-    emit('change value', {'data' : random.randrange(0,40)})
-    cur = con.cursor()
-    cur.execute("""SELECT datname from pg_database""")
-    cur.execute("INSERT INTO datname(Temperatura) VALUES (10)")
-    rows = cur.fetchall()
-    for row in rows:
-        print("   ", row[0])
+@socketio.on('start-receiving-data') # event fired by client
+def startSendingDataToClient():
+    print("Client connesso")
+
+    timeInSeconds = 0
+    
+    while True:
+        timeInSeconds += 0.1
+        
+        temperatura = random.randrange(0,40)
+        atterraggio = "'0,1,2'"
+        pressione = random.randrange(0,40)
+        gps = "'32,42,377'"
+        
+        # comando = "INSERT INTO cansatdata.data"
+        # elencoCampi = " (temperatura,areaatterraggio,pressione,gps)"
+        # elencoValori = " VALUES (" + str(temperatura) + " , " + atterraggio + " , " + str(pressione) + " , " + gps + ");"
+
+        # query = comando + elencoCampi + elencoValori
+        # curs.execute(query)
+        
+        emit('cansat-data', {
+            'seconds'     : round(timeInSeconds, 1),
+            'temperatura' : temperatura,
+            'atterraggio' : atterraggio,
+            'pressione'   : pressione,
+            'gps'         : gps
+        })
+
+        time.sleep(0.1)
+        #curs.execute("SELECT * FROM cansatdata.data")
+    
+        #result = curs.fetchall()
+        
+    # for row in result:
+            # print(row, '\n')
+
 
 if __name__ == "__main__":
     socketio.run(app)
-
-con.close()
--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import "./styles/global/quadrant.sass";
 import "./styles/global/text.sass";
@@ -12,18 +12,34 @@ import Uv from "./components/Uv";
 import Header from "./components/Header";
 import Pressure from "./components/Pressure";
 
+import io from "socket.io-client";
+import axios from "axios";
+
 const App = () => {
+  const socket = io.connect("http://localhost:5000");
+
+  const startReceivingDataFromServer = () => {
+    socket.emit("start-receiving-data");
+  };
+
+  useEffect(() => {
+    socket.on("cansat-data", data => {
+      console.log(data);
+    });
+  }, []);
+
   return (
     <div>
       <Header />
 
       <div className="quadrants-container">
+        <button onClick={startReceivingDataFromServer}>Start receiving data</button>
         <Altitude value="20" />
         <AirTemperature value="30" />
         <Pressure value={20} />
         <Uv value={20} />
-        <Status/>
-        <LaunchStatus ConnectionStatusVar={true}/>
+        <Status />
+        <LaunchStatus ConnectionStatusVar={true} />
       </div>
     </div>
   );
