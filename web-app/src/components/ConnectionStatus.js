68fd7c579f985f7ddd8b8c7a7c2b3feff980d447
angeloov <voicuangelo04@gmail.com>
2021-05-18 15:21:41 +0200

    minor fix

--- web-app/src/components/ConnectionStatus.js
+++ web-app/src/components/ConnectionStatus.js
@@ -1,31 +1,18 @@
 import React from "react";
 
 import "./ConnectionStatus.sass";
+import CanIcon from "../assets/images/can-icon.jpg";
 
-/* var ConnectionStatusVar = props => {true/false}; 
-
-var Connectionestablished = "Cansat connected";
-var ConnectioNotEstablished = "Cansat disconnected";
-
-var print; 
-
-if (ConnectionStatusVar == true){
-  print = Connectionestablished;
-}
-else {
-  print = ConnectioNotEstablished;
-}
-*/
-function Status({ConnectionStatusVar}) {
+function Status({ ConnectionStatusVar }) {
   let isConnected = true;
 
-  return <div id="ConnectionStatus">
-    <img src=".../web-app/src/Image/can-icon.jpg"></img>
-    /*
-      <div id="Status">{{background-color: (isConnected ? "green" : "red")}}</div> 
-    */
-    
-  </div>;
+  return (
+    <div id="ConnectionStatus">
+      <img src={CanIcon} />
+
+      <div id="Status" style={{ backgroundColor: isConnected ? "green" : "red" }}></div>
+    </div>
+  );
 }
 
 export default Status;
